a0f7a859919f761a6a797125beeb7d058184698b
DannyChiquillo <87905643+DannyChiquillo@users.noreply.github.com>
2021-08-21 11:39:17 -0600

    primer commit

---
+++ Danny Alexander Chiquillo Ramirez/Division/division.py
@@ -0,0 +1,4 @@
+print("Pasos para dividir numeros enteros")
+x = "Toma las primeras cifras del dividendo, el mismo número de cifras que tenga el divisor"
+y = "Divide el número del dividendo entre la cifra del divisor"
+z = "Multiplica la cifra del cociente por el divisor, el resultado escríbelo debajo del dividendo y réstalo"
